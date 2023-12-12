e21a1f86fdbd96f44434642844e1ac78689e6f37
horunfujimoto <>
2023-12-12 16:05:50 +0900

    強制型変換(キャスト)

---
+++ src/sample/Cast.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class Cast {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int number1 = 7;
+	    int number2 = 2;
+	    System.out.println(number1 / number2);
+	    
+	    // number1をdouble型にキャストし、number2で割った値を出力してください
+	    System.out.println((double)number1 / number2);				
+
+	}
+
+}

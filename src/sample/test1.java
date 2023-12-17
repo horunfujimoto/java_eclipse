56e8f8366adeeff4b2f8ca2215a75e1d4d01e700
horunfujimoto <>
2023-12-17 21:29:55 +0900

    1〜10の数字の偶数のみを小さい順に上から縦に並ぶように表示させる

---
+++ src/sample/test1.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// 問題1のコード（昇順、2から）
+        for (int num = 2; num <= 10; num += 2) {
+            System.out.println(num);
+        }
+
+	}
+
+}

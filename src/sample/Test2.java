75cff1fb2b3c2dafe5db0f338882808ceb77659b
horunfujimoto <>
2023-12-17 21:38:57 +0900

    1〜10の数字の偶数のみを大きい順に上から縦に並べる

---
+++ src/sample/Test2.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int num = 10; num >= 2; num -= 2) {
+            System.out.println(num);
+        }
+
+	}
+
+}

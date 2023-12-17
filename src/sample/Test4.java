bb09908db933d5eb2a7b01953695b1cd289bf255
horunfujimoto <>
2023-12-17 21:41:00 +0900

    1〜10までの整数をカンマ区切りで横に並べる

---
+++ src/sample/Test4.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int num = 1; num <= 10; num++) {
+            System.out.print(num);
+            if (num < 10) {
+                System.out.print(", ");
+            }
+        }
+
+	}
+
+}

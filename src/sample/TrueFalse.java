c32162c4bf350999a4c590cadf835dc7d183aeb5
horunfujimoto <hello.horurun@gmail.com>
2023-12-27 12:55:24 +0900

    論理演算子「かつ」と「または」

---
+++ src/sample/TrueFalse.java
@@ -0,0 +1,26 @@
+package sample;
+
+public class TrueFalse {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	    // trueと出力されるようにしてください
+	    System.out.println(true || false);
+	    
+	    // falseと出力されるようにしてください
+	    System.out.println(false && true);
+	    
+	    // 「8 < 5」かつ「3 >= 2」の結果を出力してください
+	    System.out.println(8 < 5 && 3 >= 2);
+	    
+	    // 「8 < 5」または「3 >= 2」の結果を出力してください
+	    System.out.println(8 < 5 || 3 >= 2);
+	    
+	    // 「8 < 5」でない、の結果を出力してください
+	    System.out.println(!(8 < 5));
+
+
+	}
+
+}
